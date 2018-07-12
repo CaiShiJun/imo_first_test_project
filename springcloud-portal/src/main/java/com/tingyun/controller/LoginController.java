3abc14d88188b8e3a1f7c5eeab4ed366ddf3bac1
蔡诗珺 <caisj@tingyun.com>
2018-07-12 19:01:21 +0800

    增加了页面模板和portal的登录页面及Freemarker配置参数

---
+++ springcloud-portal/src/main/java/com/tingyun/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.tingyun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String page(){
+        return "login";
+    }
+
+}
