3f8fe7e2586e60abc54e01e60bfa4898b354345a
蔡诗珺 <caisj@tingyun.com>
2018-07-15 14:53:27 +0800

    增加了微服务名称常量接口和portal的ribbon

---
+++ springcloud-common/src/main/java/com/tingyun/cloud/servicename/MicroserviceNames.java
@@ -0,0 +1,11 @@
+package com.tingyun.cloud.servicename;
+
+public interface MicroserviceNames {
+
+    //人员权限
+    final static String MICROSERVICE_PERSONNEL = "";
+
+
+
+
+}
--- springcloud-portal/src/main/java/com/tingyun/PortalApplication.java
+++ springcloud-portal/src/main/java/com/tingyun/PortalApplication.java
@@ -2,11 +2,24 @@ package com.tingyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class PortalApplication {
+
+    /**
+     * 使用 Ribbon 只需要在配置 RestTemplate 的 @Bean 注解下方增加 @LoadBalanced 注解。
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
